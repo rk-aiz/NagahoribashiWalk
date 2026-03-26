36319825867c8b41048d7dc99a25189fb9200bec
3030846 <3030846@30308-46>
2026-03-26 15:55:57 +0900

    first commit

---
+++ src/main/java/com/example/nagahoribashi_walk/NagahoribashiWalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.nagahoribashi_walk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagahoribashiWalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NagahoribashiWalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nagahoribashi_walk/NagahoribashiWalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nagahoribashi_walk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagahoribashiWalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
