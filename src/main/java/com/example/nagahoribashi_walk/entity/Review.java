980e0135b155147718a00e11c215457424d4dfe3
3030740 <3030740@30307-40>
2026-03-27 10:37:58 +0900

    Review作成しました！

---
+++ src/main/java/com/example/nagahoribashi_walk/entity/Review.java
@@ -0,0 +1,25 @@
+package com.example.nagahoribashi_walk.entity;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author 臼井
+ */
+public class Review {
+	
+	//主キー
+	private Long id;
+	//投稿したユーザーのID（users.id 参照）
+	private Long userId;
+	//対象スポットのID（spots.id 参照）
+	private Long spotId;
+	//5段階評価（1〜5の数値）
+	private Integer rating;
+	//自由記述の口コミ本文
+	private String comment;
+	//口コミの投稿日時
+	private LocalDateTime createdAt;
+	//口コミの最終編集日時
+	private LocalDateTime updatedAt;
+
+}
