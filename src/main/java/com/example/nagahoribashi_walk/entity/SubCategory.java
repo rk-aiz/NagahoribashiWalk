fe45af5f031a01c139b4c482f238aa6c67852615
3030751 <3030751@30307-51>
2026-03-27 10:38:00 +0900

    サブカテゴリ追加

---
+++ src/main/java/com/example/nagahoribashi_walk/entity/SubCategory.java
@@ -0,0 +1,19 @@
+package com.example.nagahoribashi_walk.entity;
+
+import lombok.Data;
+
+/**
+ * @author 篠原
+ */
+@Data
+public class SubCategory {
+
+	//主キー
+	private Long id;
+	
+	//親となるカテゴリのID（categories.id 参照）
+	private Long categoryid;
+	
+	//サブカテゴリ名
+	private String name;
+}
