10a3c6d85c8c9b0a0bdd6063508d4459c538f212
3030840 <3030840@30308-40>
2026-03-27 13:12:11 +0900

    DTO作成しました。

---
+++ src/main/java/com/example/nagahoribashi_walk/dto/ReviewDTO.java
@@ -0,0 +1,26 @@
+package com.example.nagahoribashi_walk.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+/** 
+ * @author 池田
+ */
+
+@Data
+public class ReviewDTO {
+	
+	//レビュー内容
+	//r.comment
+	private String comment;
+	
+	//レビュー投稿者名
+	//u.display_name
+	private String reviewerName;
+	
+	//レビュー投稿日
+	//r.created_at
+	private LocalDateTime reviewDate;
+
+}
