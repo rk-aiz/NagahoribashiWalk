8acb54a2ee9ef6fb3b397bc4fef1370a8d0ad6ed
rk-aiz <121715371+rk-aiz@users.noreply.github.com>
2026-03-29 21:37:44 +0900

    SubCategoryServiceのimport整理

--- src/main/java/com/example/nagahoribashi_walk/service/SubCategoryService.java
+++ src/main/java/com/example/nagahoribashi_walk/service/SubCategoryService.java
@@ -1,10 +1,5 @@
 package com.example.nagahoribashi_walk.service;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-import com.example.nagahoribashi_walk.dto.SpotSummary;
-
 /**
  * サブカテゴリ関連サービスのインターフェース
  * 
