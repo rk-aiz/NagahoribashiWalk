0b13580248aa0508b540ff140fb215b13a72af3f
3030840 <3030840@30308-40>
2026-03-30 10:54:36 +0900

    SpotDetail　必要項目の追加、ReviewDTO　IDが不足していたため追加　ReviewDTO　SpotDetailの必要項目追加に伴い、新規作成　お手数ですがご確認のほどよろしくお願いいたします。

---
+++ src/main/java/com/example/nagahoribashi_walk/dto/SpotPhotoDTO.java
@@ -0,0 +1,23 @@
+package com.example.nagahoribashi_walk.dto;
+
+import lombok.Data;
+
+/** 
+ * @author 池田
+ */
+@Data
+public class SpotPhotoDTO {
+	
+	//画像ID
+	//sp.id
+	private Long id;
+	
+	//写真のURL
+	//sp.photo_url
+	private String photoUrl;
+	
+	//写真の表示順
+	//sp.display_order
+	private Integer displayOrder;
+
+}
