b5abe6146224e51f2e4f1323d628bdef1d7aae36
3030846 <3030846@30308-46>
2026-03-30 13:00:00 +0900

    list.htmlの初期動作を作成

--- src/main/java/com/example/nagahoribashi_walk/controller/SpotController.java
+++ src/main/java/com/example/nagahoribashi_walk/controller/SpotController.java
@@ -1,12 +1,18 @@
 package com.example.nagahoribashi_walk.controller;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.nagahoribashi_walk.service.SpotService;
 
 import lombok.RequiredArgsConstructor;
 
+/**
+ * @author 海津
+ */
 @Controller
 @RequiredArgsConstructor
 public class SpotController {
@@ -14,7 +20,10 @@ public class SpotController {
     private final SpotService spotService;
 
     @GetMapping("/spot/category/all")
-    public String list() {
+    public String list(@PageableDefault(size = 12) Pageable pageable, Model model) {
+    	
+    	model.addAttribute("spots", spotService.getPage(pageable));
+    	
         return "spot/list";
     } 
 }
