b5a5b8e7da341fda0f5f25c8ed349faea20f0c74
3030916 <3030916@30309-16>
2026-03-30 14:07:04 +0900

    テストコード消しました

--- src/main/java/com/example/nagahoribashi_walk/NagahoribashiWalkApplication.java
+++ src/main/java/com/example/nagahoribashi_walk/NagahoribashiWalkApplication.java
@@ -1,15 +1,10 @@
 package com.example.nagahoribashi_walk;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.nagahoribashi_walk.controller.HomeController;
-import com.example.nagahoribashi_walk.dto.SpotSummary;
-import com.example.nagahoribashi_walk.repository.SpotMapper;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,30 +12,21 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
 public class NagahoribashiWalkApplication implements CommandLineRunner {
 
-    private final HomeController homeController;
+	private final HomeController homeController;
 
-    NagahoribashiWalkApplication(HomeController homeController) {
-        this.homeController = homeController;
-    }
+	NagahoribashiWalkApplication(HomeController homeController) {
+		this.homeController = homeController;
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(NagahoribashiWalkApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(NagahoribashiWalkApplication.class, args);
+	}
 
-    @Autowired
-    private SpotMapper spotMapper;
-    
-    // アプリ起動時に実行される
-    @Override
-    public void run(String... args) throws Exception {
+	// アプリ起動時に実行される
+	@Override
+	public void run(String... args) throws Exception {
 
-        log.info("長堀橋さんぽアプリ起動");
+		log.info("長堀橋さんぽアプリ起動");
 
-        
-        List<SpotSummary> spots = spotMapper.findRecommendedSpots();
-        
-        for (SpotSummary spot : spots) {
-        	System.out.println(spot);
-        }
-    }
+	}
 }
