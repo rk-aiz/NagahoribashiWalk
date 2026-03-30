4cc087723333d9627157fa94c9d9b23fe6cac410
3030751 <3030751@30307-51>
2026-03-30 13:47:07 +0900

    Spothtml WIP

--- src/main/java/com/example/nagahoribashi_walk/controller/SpotController.java
+++ src/main/java/com/example/nagahoribashi_walk/controller/SpotController.java
@@ -1,7 +1,12 @@
 package com.example.nagahoribashi_walk.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.nagahoribashi_walk.dto.SpotSummary;
 import com.example.nagahoribashi_walk.service.SpotService;
 
 import lombok.RequiredArgsConstructor;
@@ -14,5 +19,16 @@ import lombok.RequiredArgsConstructor;
 public class SpotController {
 
     private final SpotService spotService;
+    
+    @GetMapping("/spot")
+    public String list(String keyword, Pageable pageable, Model model) {
 
+        Page<SpotSummary> page =
+                spotService.searchByKeywords(keyword, pageable);
+
+        model.addAttribute("page", page);
+        model.addAttribute("keyword", keyword);
+
+        return "spot/list";
+    }
 }
