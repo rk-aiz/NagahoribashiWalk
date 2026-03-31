ca62240adf81f3706c76111e84b094a8adb19bd3
rk-aiz <121715371+rk-aiz@users.noreply.github.com>
2026-03-31 15:30:41 +0900

    WebMVCのConfigを統合

--- src/main/java/com/example/nagahoribashi_walk/config/WebMvcConfig.java
+++ src/main/java/com/example/nagahoribashi_walk/config/WebMvcConfig.java
@@ -1,15 +1,20 @@
 package com.example.nagahoribashi_walk.config;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.example.nagahoribashi_walk.util.StringUtils;
 
-
 /**
- * 特定のパスは指定ディレクトリを参照するようにMVCを設定する
+ * //全てのコントローラーに対して共通処理を行う
+ * 
  * @author 海津
  */
 @Configuration
@@ -18,11 +23,26 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Value("${app.upload.dir}")
     private String uploadDir;
 
+    // 特定のリソースパスは指定ディレクトリを参照するようにMVCを設定する
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
         // "/uploads/**" をapplication.propertiesのapp.upload.dirにマッピング
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + StringUtils.ensureTrailingSlash(uploadDir));
+    }
+
+    // リゾルバリストに必要なリゾルバを追加
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+
+        // Pageableに対して設定を行うためのクラスであり、リゾルバ
+        PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+
+        // ページ単位に表示する件数を追加(第一引数：ページ番号、第二引数：1ページあたりの表示件数)
+        resolver.setFallbackPageable(PageRequest.of(0, 12));
+
+        // 具体的な設定をリゾルバに追加後、リストに追加
+        argumentResolvers.add(resolver);
     }
 }
