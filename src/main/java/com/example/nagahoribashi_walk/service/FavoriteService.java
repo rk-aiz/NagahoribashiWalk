d6403ad470d213bd1db36ba7b035e7275256a2f3
3030916 <3030916@30309-16>
2026-04-01 11:30:54 +0900

    お気に入り登録削除のservice(implも)記述したので確認お願いします。

--- src/main/java/com/example/nagahoribashi_walk/service/FavoriteService.java
+++ src/main/java/com/example/nagahoribashi_walk/service/FavoriteService.java
@@ -1,5 +1,11 @@
 package com.example.nagahoribashi_walk.service;
 
 public interface FavoriteService {
+	
+       //お気に入り登録
+	    void addFavorite(Long userId, Long spotId);
+	    
+	   //お気に入り削除
+	    void removeFavorite(Long userId, Long spotId);
+	}
 
-}
