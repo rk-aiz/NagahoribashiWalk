e391138cf4433f5147fda2363c665e4b18a2f170
3030846 <3030846@30308-46>
2026-04-01 15:46:53 +0900

    wip

--- src/main/java/com/example/nagahoribashi_walk/dto/UserProfile.java
+++ src/main/java/com/example/nagahoribashi_walk/dto/UserProfile.java
@@ -1,8 +1,21 @@
 package com.example.nagahoribashi_walk.dto;
 
+import java.time.LocalDateTime;
+
 import lombok.Data;
 
 @Data
 public class UserProfile {
+	
+	/** ユーザー名 */
+	private String username;
 
+	/** メールアドレス */
+	private String email;
+	
+	/** 表示名 */
+	private String displayName;
+	
+	/** 登録日 */
+	private LocalDateTime createdAt;
 }
