d18d3b53a24b6b32adf46ec81321301df1636f39
3030916 <3030916@30309-16>
2026-04-02 09:55:42 +0900

    wip

--- src/main/java/com/example/nagahoribashi_walk/repository/ReviewMapper.java
+++ src/main/java/com/example/nagahoribashi_walk/repository/ReviewMapper.java
@@ -10,19 +10,7 @@ import com.example.nagahoribashi_walk.entity.Review;
  */
 @Mapper
 public interface ReviewMapper {
-
-	 //findBySpotId(Long spotId);
 	
 	//投稿
 	void insert(Review review);
-	
-	//編集
-//	Long update(Review review);
-	
-	//削除
-//	void deletet(Long userId, Long SpotId);
-	
-	//存在確認
-	//boolean existsBySpotIdAndUserId(Long userId, Long spotId);
-
 }
