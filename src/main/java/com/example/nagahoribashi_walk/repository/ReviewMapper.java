aff3ec3d65f37078d09a669d489f5c6a7c9db690
3030916 <3030916@30309-16>
2026-04-02 09:52:55 +0900

    wip

--- src/main/java/com/example/nagahoribashi_walk/repository/ReviewMapper.java
+++ src/main/java/com/example/nagahoribashi_walk/repository/ReviewMapper.java
@@ -2,11 +2,27 @@ package com.example.nagahoribashi_walk.repository;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.nagahoribashi_walk.entity.Review;
+
 /**
  * reviewsテーブルに対応したMapperのインターフェース
  * 
  */
 @Mapper
 public interface ReviewMapper {
+
+	 //findBySpotId(Long spotId);
+	
+	//投稿
+	void insert(Review review);
+	
+	//編集
+//	Long update(Review review);
+	
+	//削除
+//	void deletet(Long userId, Long SpotId);
+	
+	//存在確認
+	//boolean existsBySpotIdAndUserId(Long userId, Long spotId);
 
 }
