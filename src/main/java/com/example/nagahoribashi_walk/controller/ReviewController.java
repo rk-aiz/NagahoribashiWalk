d51e368d4dc1a035ecc487132dbe0412752cd9ad
3030840 <3030840@30308-40>
2026-04-03 11:53:15 +0900

    ReviewForm追加、その他修正

--- src/main/java/com/example/nagahoribashi_walk/controller/ReviewController.java
+++ src/main/java/com/example/nagahoribashi_walk/controller/ReviewController.java
@@ -1,15 +1,45 @@
 package com.example.nagahoribashi_walk.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.nagahoribashi_walk.entity.Review;
 import com.example.nagahoribashi_walk.service.ReviewService;
 
 import lombok.RequiredArgsConstructor;
+
+/**
+ * レビュー関連コントローラー
+ */
 
 @Controller
 @RequiredArgsConstructor
 public class ReviewController {
 
     private final ReviewService reviewService;
+    
+    /**
+     * レビュー投稿処理
+     */
+    
+    @PostMapping("/spot/{id}/reviews")
+    public String addReview(
+    		@PathVariable Long id,Review review,
+    		RedirectAttributes redirectAttributes) {
+    	//スポットidをセット
+    	review.setSpotId(id);
+    	
+    	try {
+        	//Service呼び出し
+    		reviewService.addReview(review); // ←失敗したらthrow
+    	} catch (Exception e) {
+    		redirectAttributes.addFlashAttribute("errorMassage", e.getLocalizedMessage());
+    	} 
+    	
+    	//投稿後は詳細画面にリダイレクト
+    	return "redirect:/spot/" + id;
+    }
 
 }
--- src/main/java/com/example/nagahoribashi_walk/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/example/nagahoribashi_walk/service/impl/ReviewServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.nagahoribashi_walk.service.impl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.nagahoribashi_walk.entity.Review;
 import com.example.nagahoribashi_walk.repository.ReviewMapper;
 import com.example.nagahoribashi_walk.service.ReviewService;
 
@@ -13,6 +14,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ReviewServiceImpl implements ReviewService {
 
-    private final ReviewMapper reviewMapper;
+	private final ReviewMapper reviewMapper;
+
+	/**
+	 * レビューを投稿する
+	 * 
+	 *@Param review レビュー情報
+	 */
+	@Override
+	public void addReview(Review review) {
+		
+		// 投稿していいか条件確認
+		
+		// 投稿できない場合↓
+		//throw new IllegalArgumentException("[投稿できない理由]のため投稿できません");
+		
+		reviewMapper.insert(review);
+	}
 
 }
