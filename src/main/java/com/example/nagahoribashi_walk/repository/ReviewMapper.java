6c2a46c6bc0bcab0c03a5edf68b961af380997ea
3030840 <3030840@30308-40>
2026-04-03 11:00:26 +0900

    修正・追加

--- src/main/java/com/example/nagahoribashi_walk/repository/ReviewMapper.java
+++ src/main/java/com/example/nagahoribashi_walk/repository/ReviewMapper.java
@@ -1,6 +1,7 @@
 package com.example.nagahoribashi_walk.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.nagahoribashi_walk.entity.Review;
 
@@ -10,14 +11,14 @@ import com.example.nagahoribashi_walk.entity.Review;
  */
 @Mapper
 public interface ReviewMapper {
-	
+
 	//投稿
 	void insert(Review review);
 
 	//編集
-	Long update(Review review);
-	
+	int update(Review review);
+
 	//削除
-	void delete(Long userId, Long SpotId);
-	
+	void delete(@Param("userId") Long userId, @Param("spotId") Long spotId);
+
 }
--- src/main/java/com/example/nagahoribashi_walk/service/ReviewService.java
+++ src/main/java/com/example/nagahoribashi_walk/service/ReviewService.java
@@ -1,6 +1,13 @@
 package com.example.nagahoribashi_walk.service;
 
+import com.example.nagahoribashi_walk.entity.Review;
+
 public interface ReviewService {
 
-	//
+	/**
+	 * レビュー情報を登録する
+	 * 
+	 * @param review レビュー情報
+	 */
+	void addReview(Review review);
 }
